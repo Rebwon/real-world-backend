3200aa310f0cdfe0f19bb40adb2ab7e62d2cf9d5
Rebwon <msolo021015@naver.com>
2021-02-09 10:38:48 +0900

    initial commit

---
+++ src/main/java/com/rebwon/realworldbackend/RealWorldBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rebwon.realworldbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealWorldBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RealWorldBackendApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/rebwon/realworldbackend/RealWorldBackendApplicationTests.java
@@ -0,0 +1,15 @@
+package com.rebwon.realworldbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class RealWorldBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
