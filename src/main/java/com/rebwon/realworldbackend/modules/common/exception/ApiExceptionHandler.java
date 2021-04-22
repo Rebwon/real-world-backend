ba0e281632c3ca18e3706890f1132929e8fd62d8
Rebwon <msolo021015@naver.com>
2021-04-22 11:28:22 +0900

    refactor code

--- src/main/java/com/rebwon/realworldbackend/modules/common/exception/ApiExceptionHandler.java
+++ src/main/java/com/rebwon/realworldbackend/modules/common/exception/ApiExceptionHandler.java
@@ -4,12 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
-@RestControllerAdvice
-public class RestControllerExceptionHandler {
+@ControllerAdvice
+public class ApiExceptionHandler {
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleMethodArgumentNotValid(
